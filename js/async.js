36e5cd4d2fa2d3c3a7bd1641c215b46559bfbff8
kangtaewan <bras1717@naver.com>
2022-08-24 21:06:13 +0900

    비동기 공부

--- js/async.js
+++ js/async.js
@@ -12,3 +12,39 @@ async function fetchAuthorName(postId) {
 }
 
 fetchAuthorName(1).then((name) => console.log("name:", name));
+
+async function logName() {
+	var user = await fetchUser('domain.com/users/1');
+	if (user.id === 1) {
+		console.log(user.name);
+	}
+}
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const getDog = async () => {
+	await sleep(1000);
+	return '멍멍이';
+};
+
+const getRabbit = async () => {
+	await sleep(500);
+	return '토끼';
+};
+const getTurtle = async () => {
+	await sleep(3000);
+	return '거북이';
+};
+
+async function process() {
+	const first = await Promise.race([
+		getDog(),
+		getRabbit(),
+		getTurtle()
+	]);
+	console.log(first);
+}
+
+process();

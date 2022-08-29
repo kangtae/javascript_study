23113f13d835c531a9a361ed358d5d587fd3a4dc
kangtaewan <bras1717@naver.com>
2022-08-29 22:37:05 +0900

    else if 공부

---
+++ js/elseif.js
@@ -0,0 +1,14 @@
+const x = 1;
+if(x >= 0){
+	console.log("x는 0보다 크거나 같다")
+}else if(x > 0){
+	console.log("x는 0보다 크다")
+}
+
+if(x >= 0){
+	console.log("x는 0보다 크거나 같다")
+}else{
+	if(x > 0) {
+		console.log("x는 0보다 크다")
+	}
+}

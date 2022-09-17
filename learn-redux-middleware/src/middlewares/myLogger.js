5d73b33eec6f19eb104e637e6a1f1c536d53dc6c
kangtaewan <bras1717@naver.com>
2022-09-17 15:50:18 +0900

    미들웨어 구현

---
+++ learn-redux-middleware/src/middlewares/myLogger.js
@@ -0,0 +1,8 @@
+const myLogger = store => next => action => {
+	console.log("\tprev",store.getState());
+	const result = next(action);
+	console.log("\tnext",store.getState());
+	return result;
+}
+
+export default myLogger;

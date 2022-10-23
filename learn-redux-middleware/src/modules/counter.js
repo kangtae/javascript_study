f28d3837cf1b162e16bb84f720e451882c1a8b3a
kangtaewan <bras1717@naver.com>
2022-10-23 21:45:52 +0900

    react-thunk 히스토리1

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -13,7 +13,7 @@ export const decreaseAsync = () => ({type: DECREASE_ASYNC});
 function *increaseSaga() {
 
 }
-console.log(1223333222433333322223333333)
+console.log(122333322243333333)
 const initialState = 0;
 
 export default function counter(state = initialState, action) {
